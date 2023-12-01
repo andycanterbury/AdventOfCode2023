519249069ff08a6497c8a9b86cd50151011228af
Andy Canterbury <andrew.canterbury@ingagepartners.com>
2023-12-01 11:06:21 -0500

    Prep for Day 2

---
+++ AdventOfCode2023/Day2.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace AdventOfCode2023
+{
+    public class Day2
+    {
+        public static string[] fileData;
+
+        [SetUp]
+        public void Setup()
+        {
+            fileData = Utilities.InputLoader.LoadFile("Data/Day2.txt");
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var answer = 0;
+            Console.WriteLine(answer);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var answer = 0;
+            Console.WriteLine(answer);
+        }
+    }
+}

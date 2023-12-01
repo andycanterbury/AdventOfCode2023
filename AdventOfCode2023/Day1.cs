3bdbf2d5a2a0765931303072894d7b680648d89a
Andy Canterbury <andrew.canterbury@ingagepartners.com>
2023-12-01 11:42:01 -0500

    Shorter ugly case statement

--- AdventOfCode2023/Day1.cs
+++ AdventOfCode2023/Day1.cs
@@ -103,34 +103,25 @@ namespace AdventOfCode2023
             switch (numberString)
             {
                 case "one":
-                case "1":
                     return 1;
-                case "two": 
-                case "2":
+                case "two":
                     return 2;
                 case "three":
-                case "3":
                     return 3;
                 case "four":
-                case "4":
                     return 4;
                 case "five":
-                case "5":
                     return 5;
                 case "six":
-                case "6":
                     return 6;
                 case "seven":
-                case "7":
                     return 7;
                 case "eight":
-                case "8":
                     return 8;
                 case "nine":
-                case "9":
                     return 9;
             }
-            return 0;
+            return int.Parse(numberString);
         }
     }
 }

f42367747487725340f9ac16e373866619470eb6
Andy Canterbury <andrew.canterbury@ingagepartners.com>
2023-12-02 00:36:03 -0500

    Day 2

--- AdventOfCode2023/Day2.cs
+++ AdventOfCode2023/Day2.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode2023
 {
@@ -16,15 +17,89 @@ namespace AdventOfCode2023
         [Test]
         public void Part1()
         {
-            var answer = 0;
-            Console.WriteLine(answer);
+            var targetRed = 12;
+            var targetGreen = 13;
+            var targetBlue = 14;
+
+            var validGames = new List<GameTotals>();
+            var idSum = 0;
+
+            foreach (var game in fileData) 
+            {
+                var totals = ParseGame(game);
+                if (totals.MaxRed <= targetRed && totals.MaxGreen <= targetGreen && totals.MaxBlue <= targetBlue)
+                {
+                    validGames.Add(totals);
+                    idSum += totals.GameId;
+                }
+            }
+
+            Console.WriteLine(idSum);
         }
 
         [Test]
         public void Part2()
         {
-            var answer = 0;
-            Console.WriteLine(answer);
+            var powerSum = 0;
+            foreach (var game in fileData)
+            {
+                var totals = ParseGame(game);
+                var power = totals.MaxRed * totals.MaxGreen * totals.MaxBlue;
+                powerSum += power;
+            }
+            Console.WriteLine(powerSum);
+        }
+
+        public GameTotals ParseGame(string game)
+        {
+            var totals = new GameTotals();
+
+            //Get Game Id
+            var idStart = game.IndexOf(" ");
+            var idEnd = game.IndexOf(":");
+            totals.GameId = int.Parse(game.Substring(idStart, idEnd - idStart));
+
+            var gameData = game.Substring(idEnd+1);
+            var draws = gameData.Split(';');
+            foreach (var draw in draws)
+            {
+                var colors = draw.Split(',');
+                foreach(var color in colors)
+                {
+                    var colorTotal = int.Parse(color.Trim().Split(' ')[0]);
+                    if (color.Contains("red"))
+                    {
+                        if (totals.MaxRed < colorTotal)
+                        {
+                            totals.MaxRed = colorTotal;
+                        }
+                    }
+                    if (color.Contains("blue"))
+                    {
+                        if (totals.MaxBlue < colorTotal)
+                        {
+                            totals.MaxBlue = colorTotal;
+                        }
+                    }
+                    if (color.Contains("green"))
+                    {
+                        if (totals.MaxGreen < colorTotal)
+                        {
+                            totals.MaxGreen = colorTotal;
+                        }
+                    }
+                }
+            }
+
+            return totals;
+        }
+
+        public class GameTotals
+        {
+            public int GameId { get; set; }
+            public int MaxRed { get; set; } = 0;
+            public int MaxGreen { get; set; } = 0;
+            public int MaxBlue { get; set; } = 0;
         }
     }
 }

87aa2483af9c4ee3d832f6676841a3803b2677a6
Andy Canterbury <andrew.canterbury@ingagepartners.com>
2023-12-03 11:24:59 -0500

    Day 3

---
+++ AdventOfCode2023/Day3.cs
@@ -0,0 +1,163 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Utilities;
+
+namespace AdventOfCode2023
+{
+    public class Day3
+    {
+        public static string[] fileData;
+
+        [SetUp]
+        public void Setup()
+        {
+            fileData = Utilities.InputLoader.LoadFile("Data/Day3.txt");
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var partNumberSum = 0;
+            var schematic = new Matrix<char>(fileData.Length, fileData[0].Length);
+            for(int i = 0; i < fileData.Length; i++)
+            {
+                for(int j = 0; j < fileData[i].Length; j++)
+                {
+                    schematic[i, j] = fileData[i][j];
+                }
+            }
+
+            var currentNumber = "";
+            var isPartNumber = false;
+
+            for(int i = 0; i < fileData.Length; i++)
+            {
+                for (int j = 0; j < fileData[i].Length; j++)
+                {
+                    if (char.IsNumber(schematic[i,j]))
+                    {
+                        currentNumber += schematic[i, j];
+                        var neighbors = schematic.GetNeighborsWithDiagonals(i, j);
+                        if (neighbors.Any(n => !char.IsNumber(n.Value) && n.Value != '.' ))
+                        {
+                            isPartNumber = true;
+                        }
+                    } else
+                    {
+                        if (currentNumber != "")
+                        {
+                            if(isPartNumber)
+                            {
+                                partNumberSum += int.Parse(currentNumber);
+                                isPartNumber = false;
+                            }
+                            currentNumber = "";
+                        }
+                    }
+                }
+                if (currentNumber != "")
+                {
+                    if (isPartNumber)
+                    {
+                        partNumberSum += int.Parse(currentNumber);
+                        isPartNumber = false;
+                    }
+                    currentNumber = "";
+                }
+            }
+            Console.WriteLine(partNumberSum);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var gearRatioSum = 0;
+            var schematic = new Matrix<char>(fileData.Length, fileData[0].Length);
+            for (int i = 0; i < fileData.Length; i++)
+            {
+                for (int j = 0; j < fileData[i].Length; j++)
+                {
+                    schematic[i, j] = fileData[i][j];
+                }
+            }
+
+            var currentNumber = "";
+            var isPotentialGear = false;
+            var potentialGearHub = new MatrixLocation<char>();
+            var potentialGears = new List<PotentialGear>();
+            for (int i = 0; i < fileData.Length; i++)
+            {
+                for (int j = 0; j < fileData[i].Length; j++)
+                {
+                    if (char.IsNumber(schematic[i, j]))
+                    {
+                        currentNumber += schematic[i, j];
+                        var neighbors = schematic.GetNeighborsWithDiagonals(i, j);
+                        if (neighbors.Any(n => n.Value == '*'))
+                        {
+                            isPotentialGear = true;
+                            potentialGearHub = neighbors.First(n => n.Value == '*');
+                        }
+                    }
+                    else
+                    {
+                        if (currentNumber != "")
+                        {
+                            if (isPotentialGear)
+                            {
+                                //is this a part of any known gears?
+                                var gearHub = potentialGears.FirstOrDefault(g => g.GearHub.Row == potentialGearHub.Row && g.GearHub.Column == potentialGearHub.Column);
+                                if (gearHub != null)
+                                {
+                                    gearHub.PartNumbers.Add(int.Parse(currentNumber));
+                                } else
+                                {
+                                    potentialGears.Add(new PotentialGear { GearHub = potentialGearHub, PartNumbers = { int.Parse(currentNumber) } });
+                                }
+                                isPotentialGear = false;
+                            }
+                            currentNumber = "";
+                        }
+                    }
+                }
+                if (currentNumber != "")
+                {
+                    if (isPotentialGear)
+                    {
+                        //is this a part of any known gears?
+                        var gearHub = potentialGears.FirstOrDefault(g => g.GearHub.Row == potentialGearHub.Row && g.GearHub.Column == potentialGearHub.Column);
+                        if (gearHub != null)
+                        {
+                            gearHub.PartNumbers.Add(int.Parse(currentNumber));
+                        }
+                        else
+                        {
+                            potentialGears.Add(new PotentialGear { GearHub = potentialGearHub, PartNumbers = { int.Parse(currentNumber) } });
+                        }
+                        isPotentialGear = false;
+                    }
+                    currentNumber = "";
+                }
+            }
+
+            foreach(var gear in  potentialGears)
+            {
+                if (gear.PartNumbers.Count == 2) 
+                {
+                    gearRatioSum += (gear.PartNumbers[0] * gear.PartNumbers[1]);
+                }
+            }
+            Console.WriteLine(gearRatioSum);
+        }
+
+        public class PotentialGear
+        {
+            public List<int> PartNumbers {get; set; } = new List<int>();
+            public MatrixLocation<char> GearHub { get; set; }
+        }
+
+
+    }
+}
--- Utilities/Matrix.cs
+++ Utilities/Matrix.cs
@@ -20,33 +20,89 @@
             set { _matrix[rowIndex, colIndex] = value; }
         }
 
-
-        public List<T> GetNeighbors(int row, int col)
+        public List<MatrixLocation<T>> GetNeighbors(int row, int col)
         {
-            List<T> neighbors = new List<T>();
+            List<MatrixLocation<T>> neighbors = new List<MatrixLocation<T>>();
             // Neighbor above
             if (row > 0)
             {
-                neighbors.Add(_matrix[row - 1, col]);
+                neighbors.Add(new MatrixLocation<T> { Value = _matrix[row - 1, col], Row = row - 1, Column = col });
             }
             // Neighbor below
             if (row < _matrix.GetLength(0) - 1)
             {
-                neighbors.Add(_matrix[row + 1, col]);
+                neighbors.Add(new MatrixLocation<T> { Value = _matrix[row + 1, col], Row = row + 1, Column = col });
             }
             // Neighbor left
             if (col > 0)
             {
-                neighbors.Add(_matrix[row, col - 1]);
+                neighbors.Add(new MatrixLocation<T> { Value = _matrix[row, col - 1], Row = row, Column = col - 1 });
             }
             // Neighbor right
             if (col < _matrix.GetLength(1) - 1)
             {
-                neighbors.Add(_matrix[row, col + 1]);
+                neighbors.Add(new MatrixLocation<T> { Value = _matrix[row, col + 1], Row = row, Column = col + 1 });
             }
 
             return neighbors;
         }
 
+        public List<MatrixLocation<T>> GetNeighborsWithDiagonals(int row, int col)
+        {
+            List<MatrixLocation<T>> neighbors = new List<MatrixLocation<T>>();
+            // Neighbors above
+            if (row > 0)
+            {
+                //Directly Above
+                neighbors.Add(new MatrixLocation<T> { Value = _matrix[row - 1, col], Row = row - 1, Column = col });
+                //Above Left
+                if(col > 0)
+                {
+                    neighbors.Add(new MatrixLocation<T> { Value = _matrix[row - 1, col - 1], Row = row - 1, Column = col - 1 });
+                }
+                //Above Right
+                if(col < _matrix.GetLength(1) - 1)
+                {
+                    neighbors.Add(new MatrixLocation<T> { Value = _matrix[row - 1, col + 1], Row = row - 1, Column = col + 1 });
+                }
+            }
+            // Neighbors below
+            if (row < _matrix.GetLength(0) - 1)
+            {
+                //Directly Below
+                neighbors.Add(new MatrixLocation<T> { Value = _matrix[row + 1, col], Row = row + 1, Column = col });
+                //Below Left
+                if (col > 0)
+                {
+                    neighbors.Add(new MatrixLocation<T> { Value = _matrix[row + 1, col - 1], Row = row + 1, Column = col - 1 });
+                }
+                //Below Right
+                if (col < _matrix.GetLength(1) - 1)
+                {
+                    neighbors.Add(new MatrixLocation<T> { Value = _matrix[row + 1, col + 1], Row = row + 1, Column = col + 1 });
+                }
+
+            }
+            // Neighbor left
+            if (col > 0)
+            {
+                neighbors.Add(new MatrixLocation<T> { Value = _matrix[row, col - 1], Row = row, Column = col - 1 });
+            }
+            // Neighbor right
+            if (col < _matrix.GetLength(1) - 1)
+            {
+                neighbors.Add(new MatrixLocation<T> { Value = _matrix[row, col + 1], Row = row, Column = col + 1 });
+            }
+
+            return neighbors;
+        }
+
+    }
+
+    public class MatrixLocation<T>
+    {
+        public T Value { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
     }
 }
